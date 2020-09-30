40b1fa774ea11896f27ab0f40d30ca62ff3148b2
TheMadSandman <martindeboutray@hotmail.fr>
2020-09-30 19:20:33 +0200

    Ajout de la page de classement

---
+++ template/classement.template.php
@@ -0,0 +1,71 @@
+<?php include "../common/header.php"; ?>
+    <h1>Classement</h1>
+    <table>
+        <tr>
+            <th>Place</th>
+            <th>Image de profil</th>
+            <th>Pseudo</th>
+            <th>Points</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="../img/profil-picture/black-sabbath.jpg" alt="PP de Ozzy Osbourne"></td>
+            <td>Ozzy Osbourne</td>
+            <td>10000</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td><img src="../img/profil-picture/dio.jpg" alt="PP de Ronnie James Dio"></td>
+            <td>Ronnie James Dio</td>
+            <td>9000</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td><img src="../img/profil-picture/motorhead.jpg" alt="PP de Lemmy Kilmister"></td>
+            <td>Lemmy Kilmister</td>
+            <td>8000</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td><img src="../img/profil-picture/iron-maiden.jpg" alt="PP de Bruce Dickinson"></td>
+            <td>Bruce Dickinson</td>
+            <td>7000</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td><img src="../img/profil-picture/judas-priest.jpg" alt="PP de Rob Halford"></td>
+            <td>Rob Halford</td>
+            <td>6000</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td><img src="../img/profil-picture/led-zeppelin.jpg" alt="PP de Chris Cornell"></td>
+            <td>Chris Cornell</td>
+            <td>5000</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td><img src="../img/profil-picture/alice-cooper.jpg" alt="PP de Alice Cooper"></td>
+            <td>Alice Cooper</td>
+            <td>4000</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td><img src="../img/profil-picture/system-of-a-down.jpg" alt="PP de Serj Tankian"></td>
+            <td>Serj Tankian</td>
+            <td>3000</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td><img src="../img/profil-picture/metallica.jpg" alt="PP de James Hetfield"></td>
+            <td>James Hetfield</td>
+            <td>2000</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td><img src="../img/profil-picture/slipknot.jpg" alt="PP de Corey Taylor"></td>
+            <td>Corey Taylor</td>
+            <td>1000</td>
+        </tr>
+    </table>
+<?php include "../common/footer.php"; ?>
---
+++ views/classement.php
@@ -0,0 +1,4 @@
+<?php
+    $title = "Classement";
+
+    include "../template/classement.template.php";
