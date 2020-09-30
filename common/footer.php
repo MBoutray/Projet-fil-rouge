3b01508c4cf621fa5000fa2814061514ec9e799b
TheMadSandman <martindeboutray@hotmail.fr>
2020-09-30 19:19:03 +0200

    Ajout des fichiers communs

---
+++ common/footer.php
@@ -0,0 +1,17 @@
+                </div> <?php //mainWrapper ?>
+            </main>
+        </div> <?php //content-container ?>
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="sondages.php">Sondages</a></li>
+                    <li><a href="classement.php">Classement</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </footer>
+    </div> <?php //page-container ?>
+</body>
+
+</html>
---
+++ common/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="page-container">
+        <div class="content-container">
+            <header>
+                <nav>
+                    <a href="#" class="logoAndName">
+                        <img class="mainLogo" src="../img/logo_BS.svg" alt="Logo BetSetter">
+                        <p>₿et$etter</p>
+                    </a>
+                    <ul>
+                        <li><a href="#">Trending</a></li>
+                        <li><a class="activeTab" href="sondages.php">Sondages</a></li>
+                        <li><a href="classement.php">Classement</a></li>
+                    </ul>
+                    <a class="connectBtn" href="#">Se connecter</a>
+                    <a class="signUpBtn" href="#">S'inscrire</a>
+                </nav>
+            </header>
+            <main>
+                <div class="mainWrapper">
