cbf712d4c573e7d62d3c291c122b88c58a92b609
TheMadSandman <martindeboutray@hotmail.fr>
2020-12-03 23:52:09 +0100

    Added new included files

--- public/include/footer.php
+++ public/include/footer.php
@@ -1,17 +1,25 @@
-                </div> <?php //mainWrapper ?>
+                </div> <?php //main-wrapper ?>
             </main>
         </div> <?php //content-container ?>
         <footer>
             <nav>
                 <ul>
-                    <li><a href="#">Trending</a></li>
-                    <li><a href="sondages.php">Sondages</a></li>
-                    <li><a href="classement.php">Classement</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="index.php?page=accueil"><img src="assets/logo_BS.svg" alt="Logo of Betsetter"></a></li>
+                    <li><a href="index.php?page=browse">Parcourir</a></li>
+                    <li><a href="index.php?page=rankings">Classement</a></li>
+                    <?php if (session_status() == PHP_SESSION_ACTIVE && $_SESSION['loggedin']) { ?>
+                        <li><a href="index.php?page=mypolls">Mes sondages</a></li>
+                    <?php } ?>
                 </ul>
             </nav>
         </footer>
     </div> <?php //page-container ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <?php
+    foreach ($js as $fileName) {
+        echo "<script src='./js/$fileName'></script>";
+    }
+    ?>
 </body>
 
 </html>
--- public/include/header.php
+++ public/include/header.php
@@ -1,30 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-
-<body>
-    <div class="page-container">
-        <div class="content-container">
-            <header>
-                <nav>
-                    <a href="#" class="logoAndName">
-                        <img class="mainLogo" src="../img/logo_BS.svg" alt="Logo BetSetter">
-                        <p>₿et$etter</p>
-                    </a>
-                    <ul>
-                        <li><a href="#">Trending</a></li>
-                        <li><a class="activeTab" href="sondages.php">Sondages</a></li>
-                        <li><a href="classement.php">Classement</a></li>
-                    </ul>
-                    <a class="connectBtn" href="#">Se connecter</a>
-                    <a class="signUpBtn" href="#">S'inscrire</a>
-                </nav>
-            </header>
-            <main>
-                <div class="mainWrapper">
+<header>
+    <nav>
+        <a href="index.php?page=home" class="logoAndName">
+            <img class="main-logo" src="./assets/logo_BS.svg" alt="Logo BetSetter">
+            <p>₿et$etter</p>
+        </a>
+        <ul>
+            <li><a href="index.php?page=browse">Parcourir</a></li>
+            <li><a href="index.php?page=rankings">Classement</a></li>
+            <?php if (session_status() == PHP_SESSION_ACTIVE && $_SESSION['loggedin']) { ?>
+                <li><a href="index.php?page=mypolls">Mes sondages</a></li>
+                <li class="online-buttons">
+                    <a href="index.php?page=profil">Profil</a>
+                    <hr class="vertical-separator">
+                    <form action="index.php" method="post">
+                        <button type="submit" name="action" value="deconnect">Déconnexion</button>
+                    </form>
+                </li>
+            <?php } else { ?>
+                <li class="offline-buttons">
+                    <a href="index.php?page=connection">Connexion</a>
+                    <hr class="vertical-separator">
+                    <a href="index.php?page=signup">Inscription</a>
+                </li>
+            <?php } ?>
+        </ul>
+    </nav>
+</header>
